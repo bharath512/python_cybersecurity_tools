09c45f28283434af4f6706fba9722af8339029d0
Bharath <aakashbharath512@gmail.com>
2025-05-20 11:05:43 -0400

    included parser.error to handle input errors

--- macchanger.py
+++ macchanger.py
@@ -7,7 +7,12 @@ def get_argument():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface name to change MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("Please enter interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("Please enter new MAC, use --help for more info")
+    return options
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address of " + interface + " to " + new_mac + "\n")
@@ -17,5 +22,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface])
     print("\n[+] Changed MAC address of " + interface + " to " + new_mac)
 
-(options, arguments) = get_argument()
+options = get_argument()
 change_mac(options.interface, options.new_mac)
