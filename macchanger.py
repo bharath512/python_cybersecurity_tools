e627476092c66611e920d28d06596f3275d2c07e
Bharath <aakashbharath512@gmail.com>
2025-05-20 10:31:30 -0400

    Commit

--- macchanger.py
+++ macchanger.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python
+
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i","--interface",dest="interface",help="Interface name to change MAC address")
+parser.add_option("-m","--new_mac",dest="new_mac",help="New MAC address")
 
 interface=input("Interface > ")
-mac_address=input("New MAC address > ")
+new_mac=input("New MAC address > ")
 
-print("[+] Changing MAC address of "+interface+" to "+mac_address+"\n")
+print("[+] Changing MAC address of " + interface +" to " + new_mac + "\n")
 subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig",interface,"hw","ether",mac_address])
+subprocess.call(["ifconfig", interface,"hw","ether", new_mac])
 subprocess.call(["ifconfig",interface,"up"])
 subprocess.call(["ifconfig",interface])
-print("\n[+] Changed MAC address of "+interface+" to "+mac_address)
+print("\n[+] Changed MAC address of " + interface +" to " + new_mac)
