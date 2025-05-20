7da406e475a5b7cc08d3fd6f7cb4025b9fcf9002
Bharath <aakashbharath512@gmail.com>
2025-05-20 10:06:27 -0400

    Commit

--- macchanger.py
+++ macchanger.py
@@ -3,6 +3,9 @@ import subprocess
 interface=input("Interface > ")
 mac_address=input("New MAC address > ")
 
+print("[+] Changing MAC address of "+interface+" to "+mac_address+"\n")
 subprocess.call(["ifconfig",interface,"down"])
 subprocess.call(["ifconfig",interface,"hw","ether",mac_address])
 subprocess.call(["ifconfig",interface,"up"])
+subprocess.call(["ifconfig",interface])
+print("\n[+] Changed MAC address of "+interface+" to "+mac_address)
