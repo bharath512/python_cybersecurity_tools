67d2ffb237eaded249115e869983b9ec15731a2a
Bharath <aakashbharath512@gmail.com>
2025-05-22 07:35:28 -0400

    Commit

--- network_scanner.py
+++ network_scanner.py
@@ -5,4 +5,12 @@ import scapy.all as scapy
 def scan(ip):
     scapy.arping(ip)
 
-scan("192.168.221.2")
+scan("192.168.221.2/24")
+
+
+
+
+#########################################################################################
+# Note
+# scapy.arping() module will not work with python2 version. It will get frozen
+#########################################################################################
